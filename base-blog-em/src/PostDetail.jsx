25a6820410234f7d0852259a03cd6324d792b6e8
Kleavant James Olmedo <kleavant@billowsoftware.com.au>
2021-11-09 18:48:56 +0800

    added react-query

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,3 +1,5 @@
+import { useMutation, useQuery } from 'react-query';
+
 async function fetchComments(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
@@ -8,7 +10,7 @@ async function fetchComments(postId) {
 async function deletePost(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/postId/${postId}`,
-    { method: "DELETE" }
+    { method: 'DELETE' }
   );
   return response.json();
 }
@@ -16,22 +18,65 @@ async function deletePost(postId) {
 async function updatePost(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/postId/${postId}`,
-    { method: "PATCH", data: { title: "REACT QUERY FOREVER!!!!" } }
+    { method: 'PATCH', data: { title: 'REACT QUERY FOREVER!!!!' } }
   );
   return response.json();
 }
 
 export function PostDetail({ post }) {
-  // replace with useQuery
-  const data = [];
+  const { data, isLoading, isError, error } = useQuery(
+    ['comment', post.id],
+    () => fetchComments(post.id)
+  );
+
+  const deleteMutation = useMutation(postId => deletePost(postId));
+  const updateMutation = useMutation(postId => updatePost(postId));
+
+  if (isLoading) {
+    return (
+      <div>
+        <h3>Loading...</h3>
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div>
+        <h3>Oops, something went wrong!</h3>
+        <p>{error.message}</p>
+      </div>
+    );
+  }
 
   return (
     <>
-      <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
+      <h3 style={{ color: 'blue' }}>{post.title}</h3>
+      <button onClick={() => deleteMutation.mutate(post.id)}>Delete</button>
+      {deleteMutation.isError && (
+        <p style={{ color: 'red' }}>Error deleting the post</p>
+      )}
+      {deleteMutation.isLoading && (
+        <p style={{ color: 'red' }}>Deleting the post</p>
+      )}
+      {deleteMutation.isSuccess && (
+        <p style={{ color: 'green' }}>Post has (not) been deleted</p>
+      )}
+      <button onClick={() => updateMutation.mutate(post.id)}>
+        Update title
+      </button>
+      {updateMutation.isError && (
+        <p style={{ color: 'red' }}>Error updating the post</p>
+      )}
+      {updateMutation.isLoading && (
+        <p style={{ color: 'red' }}>Updating the post</p>
+      )}
+      {updateMutation.isSuccess && (
+        <p style={{ color: 'green' }}>Post has (not) been updated</p>
+      )}
       <p>{post.body}</p>
       <h4>Comments</h4>
-      {data.map((comment) => (
+      {data.map(comment => (
         <li key={comment.id}>
           {comment.email}: {comment.body}
         </li>
--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,41 +1,83 @@
-import { useState } from "react";
+import { useEffect, useState } from 'react';
+import { useQuery, useQueryClient } from 'react-query';
 
-import { PostDetail } from "./PostDetail";
+import { PostDetail } from './PostDetail';
 const maxPostPage = 10;
 
-async function fetchPosts() {
+async function fetchPosts(pageNum) {
   const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`
   );
   return response.json();
 }
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    if (currentPage < maxPostPage) {
+      const nextPage = currentPage + 1;
+      queryClient.prefetchQuery(['posts', nextPage], () =>
+        fetchPosts(nextPage)
+      );
+    }
+  }, [currentPage, queryClient]);
+
   // replace with useQuery
-  const data = [];
+  const { isLoading, data, isError, error } = useQuery(
+    ['posts', currentPage],
+    () => fetchPosts(currentPage),
+    {
+      staleTime: 2000,
+      keepPreviousData: true,
+    }
+  );
+
+  if (isLoading) {
+    return (
+      <div>
+        <h3>Loading...</h3>
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div>
+        <h3>Oops, something went wrong!</h3>
+        <p>{error.message}</p>
+      </div>
+    );
+  }
 
   return (
     <>
       <ul>
-        {data.map((post) => (
+        {data.map(post => (
           <li
             key={post.id}
-            className="post-title"
+            className='post-title'
             onClick={() => setSelectedPost(post)}
           >
             {post.title}
           </li>
         ))}
       </ul>
-      <div className="pages">
-        <button disabled onClick={() => {}}>
+      <div className='pages'>
+        <button
+          disabled={currentPage <= 1}
+          onClick={() => setCurrentPage(page => page - 1)}
+        >
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button
+          disabled={currentPage >= maxPostPage}
+          onClick={() => setCurrentPage(page => page + 1)}
+        >
           Next page
         </button>
       </div>
